4cfcaf834b8861333a69194904fac4b0dd8333bf
rmoore1038 <rakinmoore3@gmail.com>
2023-08-30 13:29:11 -0500

    added week 2 files and reorganized folders

---
+++ week2/js/script.js
@@ -0,0 +1,9 @@
+ /*var heading1 = document.getElementById("heading1");
+console.log(heading1);
+heading1.innerHTML = "JavaScript Rules!";*/
+var paragraph = document.getElementById("paragraph");
+var btn = document.getElementById("btn");
+var content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis aperiam adipisci enim non harum maiores modi, repellendus quae voluptatibus, veritatis ad, optio debitis quos ea earum eius. Omnis, amet voluptatibus?";
+function showparagraph() {
+    paragraph.innerHTML = content;
+}
